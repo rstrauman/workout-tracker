e16cc18d217578452fe49bac59587253f699fc5f
Riley <riley.s2003@hotmail.com>
2026-04-27 08:05:59 -0500

    minor html tweaks

--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -1,7 +1,7 @@
 import styles from "./Login.module.css";
 import lightGym from "../../assets/Gym-Light.jpg";
 
-function LoginForm({ setEmail, setPassword, hanldeSignup, toggle }) {
+function RegisterForm({ setEmail, setPassword, hanldeSignup, toggle }) {
   return (
     <div className={styles.formCard}>
         <img src={logo} alt="SRP Fitness Logo" className={styles.logo-reg}/>  
@@ -16,10 +16,10 @@ function LoginForm({ setEmail, setPassword, hanldeSignup, toggle }) {
         <div className={styles.loginDetails}>
             <h2>Register</h2>
             <p className={styles.subtitle}>Please enter an email address and choose a password.</p>
-            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <input type="email" placeholder="Email" required onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
             <p className={styles.subtitle}>Password must be at least 8 characters long, and requires 1 Capital Letter, 1 Lowercase Letter, 1 Number, and 1 Special Character</p>
-            <button onClick={hanldeSignup}>Login</button>
+            <button onClick={hanldeSignup}>Create Account</button>
             <p className={styles.toggleText}>Already have an account? <span onClick={toggle}>Login</span></p>
         </div>
     </div>
