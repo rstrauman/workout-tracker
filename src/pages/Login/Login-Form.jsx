54cc3f12dd2265e3fbbec2f1f4c4c391416d5aa6
Riley <riley.s2003@hotmail.com>
2026-04-27 08:00:31 -0500

    added html for register page

--- src/pages/Login/Login-Form.jsx
+++ src/pages/Login/Login-Form.jsx
@@ -5,30 +5,30 @@ import darkGym from "../../assets/Gym_Background.png";
 function LoginForm({ setEmail, setPassword, handleLogin, toggle }) {
   return (
     <div className={styles.formCard}>
-      <img src={logo} alt="SRP Fitness Logo" className={styles.logo}/>
-      <div className={styles.loginDetails}>
-        <h2>Welcome Back</h2>
-        <p className={styles.subtitle}>Please enter your login details</p>
-        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-        <button className={styles.mainBtn} onClick={handleLogin}>Sign in</button>
-        <p className={styles.toggleText}>
-          Don't have an account? <span onClick={toggle}>Sign up</span>
-        </p>
-        <p className={styles.toggleText}>
-          Forgot Password? Click <span onClick={toggle}>here</span> to reset your password.
-        </p>
-      </div>
-      <div className={styles.rightImage}>
-        <img src={darkGym} alt="Gym Background" />
-        <div className={styles.imageOverlay}>
-            <p>Track every set.</p> 
-            <p>Analyze every rep.</p> 
-            <p>Own <span>your</span> progress.</p>
+        <img src={logo} alt="SRP Fitness Logo" className={styles.logo}/>
+        <div className={styles.loginDetails}>
+            <h2>Welcome Back</h2>
+            <p className={styles.subtitle}>Please enter your login details</p>
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+            <button className={styles.mainBtn} onClick={handleLogin}>Sign in</button>
+            <p className={styles.toggleText}>
+            Don't have an account? <span onClick={toggle}>Sign up</span>
+            </p>
+            <p className={styles.toggleText}>
+            Forgot Password? Click <span onClick={toggle}>here</span> to reset your password.
+            </p>
         </div>
-      </div>
+        <div className={styles.rightImage}>
+            <img src={darkGym} alt="Gym Background"/>
+            <div className={styles.imageOverlay}>
+                <p>Track every set.</p> 
+                <p>Analyze every rep.</p> 
+                <p>Own <span>your</span> progress.</p>
+            </div>
+        </div>
     </div>
-  );
+    );
 }
 
 export default LoginForm;
--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -1,13 +1,27 @@
 import styles from "./Login.module.css";
+import lightGym from "../../assets/Gym-Light.jpg";
 
 function LoginForm({ setEmail, setPassword, hanldeSignup, toggle }) {
   return (
     <div className={styles.formCard}>
-      <h2>Register</h2>
-      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={hanldeSignup}>Login</button>
-      <p>Already have an account? <span onClick={toggle}>Login</span></p>
+        <img src={logo} alt="SRP Fitness Logo" className={styles.logo-reg}/>  
+        <div className={styles.left-image}>
+            <img src={lightGym} alt="Gym Background"/>
+            <div className={styles.imageOverlay}>
+                <p>Start the JOURNEY.</p>
+                <p>Build the HABIT.</p>
+                <p>Join the ELITE.</p>
+            </div>
+        </div>  
+        <div className={styles.loginDetails}>
+            <h2>Register</h2>
+            <p className={styles.subtitle}>Please enter an email address and choose a password.</p>
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <p className={styles.subtitle}>Password must be at least 8 characters long, and requires 1 Capital Letter, 1 Lowercase Letter, 1 Number, and 1 Special Character</p>
+            <button onClick={hanldeSignup}>Login</button>
+            <p className={styles.toggleText}>Already have an account? <span onClick={toggle}>Login</span></p>
+        </div>
     </div>
   );
 }
