520aaffa098f1d7439dc340d5ab71962c675d039
Riley <riley.s2003@hotmail.com>
2026-04-28 18:05:42 -0500

    more styling for profile card

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,12 +5,13 @@ import { doc, setDoc, getDoc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
 import styles from "./Profile.module.css"; 
 
-function Profile({isOnboarding = false}) {
+function Profile({isOnboarding = false, isMetric = false}) {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [weight, setWeight] = useState("");
     const [height, setHeight] = useState("");
     const [email, setEmail] = useState("");
+    const [tel, setTel] = useState("");
     const [goal, setGoal] = useState("Hypertrophy");
     const [activityLevel, setActivityLevel] = useState("Active");
 
@@ -27,7 +28,8 @@ function Profile({isOnboarding = false}) {
                     setLastName(data.lastName || "");
                     setWeight(data.weight || "");
                     setHeight(data.height || "");
-                    setEmail(data.Email || "");
+                    setEmail(data.email || "");
+                    setTel(data.tel || "")
                     setGoal(data.goal || "Hypertrophy");
                     setActivityLevel(data.activityLevel || "Moderate");
                 }
@@ -47,6 +49,7 @@ function Profile({isOnboarding = false}) {
                 weight: Number(weight), 
                 height: Number(height),
                 email,
+                tel,
                 goal,
                 activityLevel, 
                 isProfileComplete: true, 
@@ -80,69 +83,89 @@ function Profile({isOnboarding = false}) {
                     Edit Profile ✎
                 </button>
             )}
+            {!isOnboarding && isEditing && (
+                <button onClick={() => setIsEditing(false)} className={styles.exitIcon}>
+                    X
+                </button>
+            )}
             <div className={styles.contentContainer}>
-                {/* <div className={styles.form}> */}
+                <div className={styles.stats}>
+                    <input 
+                        placeholder="First Name" 
+                        value={firstName}
+                        disabled={!isEditing}
+                        onChange={(e) => setFirstName(e.target.value)} 
+                    />
+                    <input 
+                        placeholder="Last Name" 
+                        value={lastName}
+                        disabled={!isEditing}
+                        onChange={(e) => setLastName(e.target.value)} 
+                    />
+                </div>
+                <div className={styles.stats}>
+                    <input 
+                        placeholder="Weight (kg)" 
+                        value={isMetric ? `${weight} kg` : `${Math.round(weight * 2.20462)} lbs`}
+                        disabled={!isEditing}
+                        onChange={(e) => setWeight(e.target.value)} 
+                    />
+                    <input 
+                        placeholder="Height (cm)" 
+                        value={isMetric ? `${height} cm` : `${Math.floor(height / 2.54 / 12)}' ${Math.round((height / 2.54) % 12)}"`}
+                        disabled={!isEditing}
+                        onChange={(e) => setHeight(e.target.value)} 
+                    />
+                </div>
+                {!isOnboarding && (
                     <div className={styles.stats}>
-                        <input 
-                            placeholder="First Name" 
-                            value={firstName}
-                            disabled={!isEditing}
-                            onChange={(e) => setFirstName(e.target.value)} 
-                        />
-                        <input 
-                            placeholder="Last Name" 
-                            value={lastName}
-                            disabled={!isEditing}
-                            onChange={(e) => setLastName(e.target.value)} 
-                        />
-                    </div>
-                    <div className={styles.stats}>
-                        <input 
-                            placeholder="Weight (kg)" 
-                            value={weight}
-                            disabled={!isEditing}
-                            onChange={(e) => setWeight(e.target.value)} 
-                        />
-                        <input 
-                            placeholder="Height (cm)" 
-                            value={height}
-                            disabled={!isEditing}
-                            onChange={(e) => setHeight(e.target.value)} 
-                        />
-                    </div>
-                    {!isOnboarding && (
-                        <div className={styles.stats}>
+                        <div className={`${styles.inputGroup} ${styles.fullWidth}`}>
                             <input 
                                 type="email" 
                                 placeholder="Email" 
                                 value={email} 
                                 disabled={!isEditing} 
-                                onChange={(e) => setEmail(e.target.value)}/>
-                        </div>
-                    )}
-                    <div className={styles.stats}>
-                        <div className={styles.inputGroup}>
-                            <label>Training Goal</label>
-                            <select value={goal} disabled={!isEditing} onChange={(e) => setGoal(e.target.value)}>
-                                <option value="Hypertrophy">Hypertrophy (Muscle Gain)</option>
-                                <option value="Strength">Strength (Powerlifting)</option>
-                                <option value="Maintenance">Maintenance</option>
-                                <option value="Fat Loss">Fat Loss (Cutting)</option>
-                                <option value="Athletic Performance">Athletic Performance</option>
-                            </select>
-                        </div>
-                        <div className={styles.inputGroup}>
-                            <label>Activity Level</label>
-                            <select id="activityLevel" value={activityLevel} disabled={!isEditing} onChange={(e) => setActivityLevel(e.target.value)} className={styles.dropdown}>
-                                <option value="Sedentary">Sedentary (Little to no exercise)</option>
-                                <option value="Light">Light (Light exercise 1-3x per week or a Job where you spend time on your feet)</option>
-                                <option value="Moderate">Moderate (Moderate exercise 3-5x per week)</option>
-                                <option value="Vigorous">Very (Moderate-Vigorous exercise 6-7x per week, includes running or sports)</option>
-                                <option value="Extra">Extra (Vigorous training 2x per day, have a job that requires hard physical labour)</option>
-                            </select>
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                     </div>
-                {/* </div> */}
+                )}
+                {!isOnboarding && (
+                    <div className={styles.stats}>
+                        <div className={`${styles.inputGroup} ${styles.fullWidth}`}>
+                            <input 
+                                type="tel" 
+                                placeholder="Phone Number" 
+                                value={tel} 
+                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                disabled={!isEditing} 
+                                onChange={(e) => setTel(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                )}
+                <div className={styles.stats}>
+                    <div className={styles.inputGroup}>
+                        <label>Training Goal</label>
+                        <select value={goal} disabled={!isEditing} onChange={(e) => setGoal(e.target.value)}>
+                            <option value="Hypertrophy">Hypertrophy (Muscle Gain)</option>
+                            <option value="Strength">Strength (Powerlifting)</option>
+                            <option value="Maintenance">Maintenance</option>
+                            <option value="Fat Loss">Fat Loss (Cutting)</option>
+                            <option value="Athletic Performance">Athletic Performance</option>
+                        </select>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label>Activity Level</label>
+                        <select id="activityLevel" value={activityLevel} disabled={!isEditing} onChange={(e) => setActivityLevel(e.target.value)} className={styles.dropdown}>
+                            <option value="Sedentary">Sedentary (Little to no exercise)</option>
+                            <option value="Light">Light (Light exercise 1-3x per week or a Job where you spend time on your feet)</option>
+                            <option value="Moderate">Moderate (Moderate exercise 3-5x per week)</option>
+                            <option value="Vigorous">Very (Moderate-Vigorous exercise 6-7x per week, includes running or sports)</option>
+                            <option value="Extra">Extra (Vigorous training 2x per day, have a job that requires hard physical labour)</option>
+                        </select>
+                    </div>
+                </div>
             </div>
             <div className={styles.stats}>
                 <button onClick={handleSave}>Save Profile</button>
