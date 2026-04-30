097e94932e8fff45dad240d888acca8244909a3b
Riley <riley.s2003@hotmail.com>
2026-04-30 15:13:30 -0500

    started layout for dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 function Dashboard() {
     return (
-        <div style={{ color: 'white', padding: '50px', textAlign: 'center' }}>
+        <div>
             <h1>Home</h1>
             <p>This is the Home Page</p>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Workout</li>
+                    <li>Meals</li>
+                    <li>Progress</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
         </div>
     );
 }
